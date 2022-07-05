0a3c05ca0d3f5356422ed6e5c2a03476f764e0b1
BD103 <59022059+BD103@users.noreply.github.com>
2022-07-05 08:32:56 -0400

    feat: allow toggling dev dependencies

--- src/dev.rs
+++ src/dev.rs
@@ -1,11 +1,13 @@
 use bevy::{input::system::exit_on_esc_system, prelude::*};
-use bevy_inspector_egui::WorldInspectorPlugin;
+{% if bevy_inspector_egui %}use bevy_inspector_egui::WorldInspectorPlugin;{% endif %}
 
 pub struct DevPlugin;
 
 impl Plugin for DevPlugin {
     fn build(&self, app: &mut App) {
+        {%- if bevy_inspector_egui %}
         app.add_plugin(WorldInspectorPlugin::new())
             .add_system(exit_on_esc_system);
+        {%- endif %}
     }
 }
